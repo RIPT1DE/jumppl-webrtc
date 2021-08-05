04408b9b60eddcf30e97c673c522966861b9bf64
Talha Siddiqi <siddiqi.talha1@gmail.com>
2021-08-05 11:39:35 +0500

    added some frontend logic

--- public/index.js
+++ public/index.js
@@ -2,6 +2,12 @@ console.log('workin');
 
 const socket = io();
 
+let pc = null;
+let ignoreOffer = false;
+let makingOffer = false;
+let polite = false;
+let userId = 'asd';
+
 // client-side
 socket.on('connect', () => {
   console.log(socket.id);
@@ -14,6 +20,7 @@ socket.on('disconnect', () => {
 function join() {
   console.log('join');
   const id = document.getElementById('userId').value;
+  userId = id;
   if (id) {
     socket.emit('join', id);
     console.log(id);
@@ -48,11 +55,76 @@ socket.on('callActive', (d) => {
 
   if (d.userId) {
     document.getElementById('call').style.color = '#ff0000';
+    pc = new RTCPeerConnection({
+      iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+    });
+    addHandlers();
+    polite = userId.localeCompare(d.userId) > 0;
   } else {
     document.getElementById('call').style.color = '#000000';
   }
 });
 
-socket.on('callMessage', (data) => {
-  console.log('message: ', data);
+socket.on('callMessage', async ({ description, candidate }) => {
+  console.log('message: ', description, candidate);
+  if (pc) {
+    try {
+      if (description) {
+        const offerCollision =
+          description.type == 'offer' &&
+          (makingOffer || pc.signalingState != 'stable');
+
+        ignoreOffer = !polite && offerCollision;
+        if (ignoreOffer) {
+          return;
+        }
+
+        await pc.setRemoteDescription(description);
+        if (description.type == 'offer') {
+          await pc.setLocalDescription();
+          socket.emit('callMessage', { description: pc.localDescription });
+        }
+      } else if (candidate) {
+        try {
+          await pc.addIceCandidate(candidate);
+        } catch (err) {
+          if (!ignoreOffer) {
+            throw err;
+          }
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
 });
+
+function addHandlers() {
+  pc.onnegotiationneeded = async () => {
+    try {
+      makingOffer = true;
+      await pc.setLocalDescription();
+      socket.emit('callMessage', { description: pc.localDescription });
+    } catch (err) {
+      console.error(err);
+    } finally {
+      makingOffer = false;
+    }
+  };
+
+  pc.onicecandidate = ({ candidate }) =>
+    socket.emit('callMessage', { candidate });
+
+  let remoteVideo = document.getElementById('remoteVideo');
+
+  pc.ontrack = ({ track, streams }) => {
+    track.onunmute = () => {
+      console.log('track');
+      if (remoteVideo.srcObject) {
+        return;
+      }
+      console.log('adding track');
+      remoteVideo.srcObject = streams[0];
+    };
+  };
+}
